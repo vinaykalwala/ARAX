ca2a3891041e2e4327c6ee35de82e0507d676470
Durga Prakash Shilarapu <109608138+Durgaprakash1102@users.noreply.github.com>
2025-06-04 16:06:21 +0530

    Entire Project SetUp

--- ARAX/urls.py
+++ ARAX/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for ARAX project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from AraxMSS.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('aboutus/',about_us, name='about_us'),
+    path('services/',services, name='services'),
+    path('gallery/', gallery, name='gallery'),
+    path('contactus/',contact_us, name='contact_us'),
 ]
--- AraxMSS/views.py
+++ AraxMSS/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about_us(request):
+    return render(request, 'aboutus.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact_us(request):
+    return render(request, 'contactus.html')
